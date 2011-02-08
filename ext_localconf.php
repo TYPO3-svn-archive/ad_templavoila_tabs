2207ca9ab0b55704df5fc78d2bec2a8b9896efb9
adgrafik <adgrafik@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-08 17:58:37 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ad_templavoila_tabs/trunk@43291 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_localconf.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+/**
+ * For TemplaVoila versions greater than or equal to 1.5, add static data structures as part of EXTCONF
+ * For TemplaVoila versions less than 1.5, see ext_tables.php
+ */
+
+if (t3lib_div::int_from_ver(t3lib_extMgm::getExtensionVersion('templavoila')) >= 1005000) {
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila']['staticDataStructures'][] = array(
+		'title' => 'LLL:EXT:' . $_EXTKEY . '/lang/locallang.xml:fce.typeHeader',
+		'path' => 'EXT:' . $_EXTKEY . '/core_templates/datastructures/fce/tabs (fce).xml',
+		'icon' => '',
+		'scope' => 2,
+	);
+}
+
+?>
