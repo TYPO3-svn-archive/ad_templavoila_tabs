2a850d53dc8101687e30d5b806811047e02e9fa2
adgrafik <adgrafik@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-03 15:07:54 +0000

    Bugfix release.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ad_templavoila_tabs/trunk@44564 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ad_templavoila_tabs".
 #
-# Auto generated 12-02-2011 14:48
+# Auto generated 03-03-2011 16:06
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This Extension extends Templavoila Framework with jQuery-Tabs. NOTE: Tested only for templavoila_framework! But should work with templavoila also. Please test and response ;)',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'dependencies' => 'templavoila',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"855a";s:40:"class.tx_adtemplavoilatabs_t3dimport.php";s:4:"d7c9";s:21:"ext_conf_template.txt";s:4:"0ac5";s:12:"ext_icon.gif";s:4:"3dcf";s:17:"ext_localconf.php";s:4:"8881";s:14:"ext_tables.php";s:4:"28f8";s:14:"fec_import.t3d";s:4:"ed51";s:49:"core_templates/datastructures/fce/tabs (fce).html";s:4:"7b31";s:48:"core_templates/datastructures/fce/tabs (fce).xml";s:4:"721f";s:32:"core_templates/fce/fce_tabs.html";s:4:"9325";s:39:"core_templates/typoscript/constants.txt";s:4:"19ab";s:35:"core_templates/typoscript/setup.txt";s:4:"f399";s:14:"doc/manual.sxw";s:4:"85b2";s:18:"lang/locallang.xml";s:4:"cb3d";s:22:"lang/locallang_csh.xml";s:4:"dde5";s:23:"res/js/jquery.cookie.js";s:4:"3847";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"1250";s:40:"class.tx_adtemplavoilatabs_t3dimport.php";s:4:"d7c9";s:21:"ext_conf_template.txt";s:4:"0ac5";s:12:"ext_icon.gif";s:4:"3dcf";s:17:"ext_localconf.php";s:4:"8881";s:14:"ext_tables.php";s:4:"28f8";s:14:"fec_import.t3d";s:4:"ed51";s:49:"core_templates/datastructures/fce/tabs (fce).html";s:4:"7b31";s:48:"core_templates/datastructures/fce/tabs (fce).xml";s:4:"721f";s:32:"core_templates/fce/fce_tabs.html";s:4:"9325";s:39:"core_templates/typoscript/constants.txt";s:4:"19ab";s:35:"core_templates/typoscript/setup.txt";s:4:"9744";s:14:"doc/manual.sxw";s:4:"01b2";s:18:"lang/locallang.xml";s:4:"cb3d";s:22:"lang/locallang_csh.xml";s:4:"dde5";s:23:"res/js/jquery.cookie.js";s:4:"3847";}',
 	'suggests' => array(
 	),
 );
